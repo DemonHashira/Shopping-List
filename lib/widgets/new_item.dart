21a82a0791035ef0fadf3933e9866c7ab957a376
DemonHashira <v.logodazhki@outlook.com>
2024-02-01 00:04:49 +0200

    Added post request to firebase

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:shopping_list_app/data/categories.dart';
 import 'package:shopping_list_app/models/category.dart';
-import 'package:shopping_list_app/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -22,14 +23,24 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      Navigator.of(context).pop(
-        GroceryItem(
-          id: DateTime.now().toString(),
-          name: _enteredName,
-          quantity: _enteredQuantity,
-          category: _enteredCategory,
+      final url = Uri.https(
+        'shopping-list-5795b-default-rtdb.firebaseio.com',
+        'shopping-list.json',
+      );
+      http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode(
+          {
+            'name': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _enteredCategory.title,
+          },
         ),
       );
+      // Navigator.of(context).pop();
     }
   }
 
